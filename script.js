aa96e86103d14307b3b9b4074c38f97edb26c96d
Karine Paiva <karineppaiva24@gmail.com>
2026-01-16 15:23:57 -0300

    Inserindo objeto de áudio

--- script.js
+++ script.js
@@ -5,22 +5,38 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const subtitulo = document.querySelector('.app__title-strong')
+const botoes = document.querySelectorAll('.app__card-button')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3') 
+musica.loop = true
 
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused){
+        musica.play()
+    }else {
+        musica.pause()
+    }
+})
 
 focoBt.addEventListener('click', () =>{
     alterarContexto('foco')
-    
+    focoBt.classList.add('active')
 })
 curtoBt.addEventListener('click', () =>{
     alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () =>{
     alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 //utilizou uma função para alterar o contexto da pagina
 
 function alterarContexto(contexto){
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
