8a07034f1803667d0a31173faebe5fa12c2f234a
Karine Paiva <karineppaiva24@gmail.com>
2026-01-16 12:13:41 -0300

    Separando funções e métodos

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo') //selecionou a classe do botao 
 // e armazenou em uma variavel
 
-//const titulo = document.querySelector('.app__title')
-//const subtitulo = document.querySelector('.app__title-strong')
+const displayTempo = document.querySelector('#timer')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const subtitulo = document.querySelector('.app__title-strong')
 
 
 focoBt.addEventListener('click', () =>{
-    html.setAttribute('data-contexto','foco')
-})//adicinou um evento de click ao botao focoBt que ao ser clicado
-//adiciona ao elemento html o atributo data-contexto com valor foco
-
+    alterarContexto('foco')
+    
+})
 curtoBt.addEventListener('click', () =>{
-    html.setAttribute('data-contexto','descanso-curto')
+    alterarContexto('descanso-curto')
 })
 
 longoBt.addEventListener('click', () =>{
-    html.setAttribute('data-contexto','descanso-longo')
+    alterarContexto('descanso-longo')
 })
-//mesma logica dos botoes anteriores, mas com valores diferentes para o atributo data-contexto
+//utilizou uma função para alterar o contexto da pagina
+
+function alterarContexto(contexto){
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
+
